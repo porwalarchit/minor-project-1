c524909848c173392073a31855f59322e39b877c
vishu-cloud <56029601+vishu-cloud@users.noreply.github.com>
2022-01-15 02:05:27 +0530

    Update ProductCard.js
    container added

--- frontend/src/Landingcomponents/ProductCard.js
+++ frontend/src/Landingcomponents/ProductCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card, Button} from 'react-bootstrap';
+import {Card, Button,Container} from 'react-bootstrap';
 import Landingpage from './Landingpage';
 
 function ProductCard({prod}) {
